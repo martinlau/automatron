e0ea01cc66c0ae51ce3537fc99e4fcae98542153
Martin Lau <m@hum.ph>
2014-03-06 22:11:07 +1100

    Replaced r10k with vagrant-librarian-puppet

--- vagrant/puppet/config/manifests/site.pp
+++ vagrant/puppet/config/manifests/site.pp
@@ -1,19 +1,20 @@
+
 class { 'motd':
     template => 'custom/motd.erb'
 }
 
-package { 'mariadb-server':
-    ensure => 'latest'
-}
-
-package { 'java-devel':
-    ensure => 'latest'
-}
-
-package { 'tomcat':
-    ensure => 'latest'
-}
-
-package { 'mysql-connector-java':
-    ensure => 'latest'
-}
+#package { 'mariadb-server':
+#    ensure => 'latest'
+#}
+#
+#package { 'java-devel':
+#    ensure => 'latest'
+#}
+#
+#package { 'tomcat':
+#    ensure => 'latest'
+#}
+#
+#package { 'mysql-connector-java':
+#    ensure => 'latest'
+#}
